456c926f6741649b5478c7b7f58b63a0f62601f5
Kohei Ogawa <ogawa@oysterworld.jp>
2018-09-12 18:44:32 +0900

    PHP勉強用のサンプル

--- MyVagrant-tomcat/html/sample.php
+++ MyVagrant-tomcat/html/sample.php
@@ -1,7 +1,21 @@
 <?php
-  $count = 1;
-  while($count < 6){
-    echo $count;
-    $count++;
+  $mysqli = new mysqli("localhost","root","","");
+  if ($mysqli->connect_error) {
+    echo $mysqli->connect_error;
+    exit();
+  } else {
+    $mysqli->set_charset("utf8");
   }
- ?>
+
+$mysqli->query("create database if not exists test2 default character set utf8");
+$result = $mysqli->query("show databases");
+
+while ($row = $result->fetch_array(MYSQLI_NUM)) {
+    echo $row[0];
+		echo nl2br("\n");
+}
+
+// DB接続を閉じる
+  $mysqli->close();
+
+?>
